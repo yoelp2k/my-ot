919d9d0293a706a102751814b153b64da039113b
GitHub Action <action@github.com>
2024-05-14 17:11:10 +0000

    Bump version to 1.0.16

--- script.js
+++ script.js
@@ -50,5 +50,5 @@ switchLanguage('en');
 // Version update
 document.addEventListener('DOMContentLoaded', function() {
     /** version will automatically be updated on each push by ./.github/workflows/increment-versio.yml */
-    document.getElementById('version-display').textContent = 'Version 1.0.15';
+    document.getElementById('version-display').textContent = 'Version 1.0.16';
 });
